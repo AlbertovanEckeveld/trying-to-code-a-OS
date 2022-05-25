05f122ee0f297b855a0a9e8942d1530164e5c575
AlbertoVE <eckeveld@outlook.com>
2022-05-25 18:19:49 +0200

    Upload README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Trying to code my own OS
+Q: WHAT ARE YOU TRYING TO DO WITH IT? A: idk
+
+## First of all
+I have used NASM for make from assemble code binary
+I have used qemu for testing the binary code from nasm
+
--- boot.asm
+++ boot.asm
@@ -1,3 +1,11 @@
-jmp $
-times 510-($-$$) db 0
-db 0x055, 0x0aa
+jmp $                                   ; Jump to the current memory address
+ 
+times 510-($-$$) db 0                   ; We needs this file to be 512 bits big, so we need some junk binary code to full up the file. $$ = The beginning of the current section. 
+db 0x055, 0x0aa 
+
+/* 
+
+    The "times" function: times n action. It repeats action n times
+    db = define byte. It adds one or more bytes to the binary code
+
+*/
